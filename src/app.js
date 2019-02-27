8c5894639313b049bea87bac9695d9d9e6ae455b
Dexter De Leon <dexdeleon@gmail.com>
2019-02-27 09:10:24 +0000

    React boilerplate, axios installed, frontend pulling data from backend and displaying.

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+import './scss/style.scss'
+
+class App extends React.Component {
+
+  componentDidMount(){
+    axios.get('/api/users')
+      .then(res => this.setState({ users: res.data }))
+  }
+
+  render(){
+    if (!this.state) return <h1>Calm, the jams are coming...</h1>
+    return(
+      <div>
+        <h1>The Index of Jams</h1>
+        <h2>Lovingly brought to you by...</h2>
+        {this.state.users.map(user =>
+          <h3 key={user.id}>{user.username}</h3>
+        )}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
