3573d8a2cbbdea2fed8e7dd493e1b1d1006ccd28
Dexter De Leon <dexdeleon@gmail.com>
2019-02-27 10:58:43 +0000

    Experiments with Tone.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
+import Tone from 'tone'
 
 import './scss/style.scss'
 
+
 class App extends React.Component {
+
+  constructor(){
+    super()
+
+    this.handleClick = this.handleClick.bind(this)
+  }
 
   componentDidMount(){
     axios.get('/api/users')
       .then(res => this.setState({ users: res.data }))
+
+    const that = this
+    this.loop = new Tone.Sequence((time, beat) => {
+      that.setState({transport: {beat, time}})
+    }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], '16n')
+
   }
 
   render(){
